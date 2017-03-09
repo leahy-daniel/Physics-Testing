f55e5fed6035b6df27cf507bc12670cc3eacc0af
Daniel Leahy <daniel_leahy@berkeley.edu>
2017-03-09 10:00:34 -0800

    test

---
+++ Assets/Scipts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float gravity = 9.8f;
+	public float speed = 8f;
+	public float jumpForce = 5f;
+
+	private CharacterController controller;
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent <CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.isGrounded) {
+			if (Input.GetKey(KeyCode.Space)) {
+				velocity.y = jumpForce;
+			}
+		}
+		velocity.y -= gravity * Time.deltaTime;
+		velocity.x = Input.GetAxis ("Horizontal") * speed;
+		velocity.z = Input.GetAxis ("Vertical") * speed;
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
---
+++ Assets/Scipts/PlayerMovementRigid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementRigid : MonoBehaviour {
+
+	public float speed = 8f;
+	public float jumpForce = 90f;
+
+	Vector3 velocity;
+	float distanceToGround;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		velocity = Vector3.zero;
+		rb = GetComponent<Rigidbody> ();
+		distanceToGround = GetComponent<CapsuleCollider> ().bounds.extents.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (isGrounded()) {
+			if (Input.GetKey(KeyCode.Space)) {
+				rb.AddForce (0f, jumpForce, 0f);
+			}
+		}
+		velocity.x = Input.GetAxis ("Horizontal") * speed;
+		velocity.z = Input.GetAxis ("Vertical") * speed;
+		rb.MovePosition (transform.position + velocity * Time.deltaTime);
+	}
+
+	bool isGrounded() {
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, -Vector3.up, out hit)) {
+			return hit.distance <= distanceToGround + 0.1;
+		} else {
+			return false;
+		}
+	}
+}
